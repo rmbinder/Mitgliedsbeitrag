96fdc74568bbf142a05b313995fd8311ca1af4af
rmbinder <r-m-b@gmx.net>
2017-01-14 16:11:41 +0100

    export feature for history

--- history.php
+++ history.php
@@ -11,10 +11,13 @@
  *
  * Parameters:
  *
- * filter_date_from : is set to actual date,
- *             if no date information is delivered
- * filter_date_to   : is set to 31.12.9999,
- *             if no date information is delivered
+ * mode:				Output(html, print, csv-ms, csv-oo, pdf, pdfl)
+ * filter_date_from: 	is set to actual date,
+ *             			if no date information is delivered
+ * filter_date_to: 		is set to 31.12.9999,
+ *             			if no date information is delivered
+ * full_screen:     	false - (Default) show sidebar, head and page bottom of html page
+ *                  	true  - Only show the list without any other html unnecessary elements
  ***********************************************************************************************
  */
 
@@ -37,13 +40,17 @@ $filterDateFrom = DateTime::createFromFormat('Y-m-d', DATE_NOW);
 $filterDateFrom->modify('-'.$gPreferences['members_days_field_history'].' day');
 
 // Initialize and check the parameters
-$getDateFrom = admFuncVariableIsValid($_GET, 'filter_date_from', 'date', array('defaultValue' => $filterDateFrom->format($gPreferences['system_date'])));
-$getDateTo   = admFuncVariableIsValid($_GET, 'filter_date_to',   'date', array('defaultValue' => DATE_NOW));
+$getDateFrom   = admFuncVariableIsValid($_GET, 'filter_date_from', 'date',  array('defaultValue'  => $filterDateFrom->format($gPreferences['system_date'])));
+$getDateTo     = admFuncVariableIsValid($_GET, 'filter_date_to',   'date',  array('defaultValue'  => DATE_NOW));
+$getMode       = admFuncVariableIsValid($_GET, 'mode',             'string', array('defaultValue' => 'html', 'validValues' => array('csv-ms', 'csv-oo', 'html', 'print', 'pdf', 'pdfl')));
+$getFullScreen = admFuncVariableIsValid($_GET, 'full_screen',      'bool');
 
 $headline = $gL10n->get('PLG_MITGLIEDSBEITRAG_CONTRIBUTION_HISTORY');
+$filename = $gL10n->get('PLG_MITGLIEDSBEITRAG_CONTRIBUTION_HISTORY');
 
 // Initialize local parameteres
-$sqlConditions = '';
+$sqlConditions 	= '';
+$csvStr 		= ''; // CSV file as string
 
 // add page to navigation history
 $gNavigation->addUrl(CURRENT_URL, $headline);
@@ -84,6 +91,47 @@ $dateFromHtml   = $objDateFrom->format($gPreferences['system_date']);
 $dateToIntern   = $objDateTo->format('Y-m-d');
 $dateToHtml     = $objDateTo->format($gPreferences['system_date']);
 
+// initialize some special mode parameters
+$separator   = '';
+$valueQuotes = '';
+$charset     = '';
+$classTable  = '';
+$orientation = '';
+
+switch ($getMode)
+{
+	case 'csv-ms':
+		$separator   = ';';  // Microsoft Excel 2007 or new needs a semicolon
+		$valueQuotes = '"';  // all values should be set with quotes
+		$getMode     = 'csv';
+		$charset     = 'iso-8859-1';
+		break;
+	case 'csv-oo':
+		$separator   = ',';   // a CSV file should have a comma
+		$valueQuotes = '"';   // all values should be set with quotes
+		$getMode     = 'csv';
+		$charset     = 'utf-8';
+		break;
+	case 'pdf':
+		$classTable  = 'table';
+		$orientation = 'P';
+		$getMode     = 'pdf';
+		break;
+	case 'pdfl':
+		$classTable  = 'table';
+		$orientation = 'L';
+		$getMode     = 'pdf';
+		break;
+	case 'html':
+		$classTable  = 'table table-condensed';
+		break;
+	case 'print':
+		$classTable  = 'table table-condensed table-striped';
+		break;
+	default:
+		break;
+}
+
 // create select statement with all necessary data
 $sql = 'SELECT usl_id, usl_usr_id, last_name.usd_value AS last_name, first_name.usd_value AS first_name, usl_usf_id, usl_value_new, usl_timestamp_create
           FROM '.TBL_USER_LOG.'
@@ -106,42 +154,197 @@ if($fieldHistoryStatement->rowCount() === 0)
     // => EXIT
 }
 
-// create html page object
-$page = new HtmlPage($headline);
+if ($getMode !== 'csv')
+{
+	$datatable = false;
+	$hoverRows = false;
 
-// add back link to module menu
-$profileFieldHistoryMenu = $page->getMenu();
-$profileFieldHistoryMenu->addItem('menu_item_back', $gNavigation->getPreviousUrl(), $gL10n->get('SYS_BACK'), 'back.png');
+	if ($getMode === 'print')
+	{
+		// create html page object without the custom theme files
+		$page = new HtmlPage();
+		$page->hideThemeHtml();
+		$page->hideMenu();
+		$page->setPrintMode();
+		$page->setTitle($title);
+		$page->setHeadline($headline);
+		$table = new HtmlTable('history_table', $page, $hoverRows, $datatable, $classTable);
+	}
+	elseif ($getMode === 'pdf')
+	{
+		require_once(ADMIDIO_PATH . FOLDER_LIBS_SERVER . '/tcpdf/tcpdf.php');
+		$pdf = new TCPDF($orientation, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// create filter menu with input elements for Startdate and Enddate
-$FilterNavbar = new HtmlNavbar('menu_history_filter', null, null, 'filter');
-$form = new HtmlForm('navbar_filter_form', ADMIDIO_URL . FOLDER_PLUGINS . $plugin_folder .'/history.php', $page, array('type' => 'navbar', 'setFocus' => false));
-$form->addInput('filter_date_from', $gL10n->get('SYS_START'), $dateFromHtml, array('type' => 'date', 'maxLength' => 10));
-$form->addInput('filter_date_to', $gL10n->get('SYS_END'), $dateToHtml, array('type' => 'date', 'maxLength' => 10));
-$form->addSubmitButton('btn_send', $gL10n->get('SYS_OK'));
-$FilterNavbar->addForm($form->show(false));
-$page->addHtml($FilterNavbar->show());
+		// set document information
+		$pdf->SetCreator(PDF_CREATOR);
+		$pdf->SetAuthor('Admidio');
+		$pdf->SetTitle($headline);
 
-$table = new HtmlTable('history_table', $page, true, true);
+		// remove default header/footer
+		$pdf->setPrintHeader(true);
+		$pdf->setPrintFooter(false);
+		
+		// set header and footer fonts
+		$pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+		$pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
+		// set auto page breaks
+		$pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+		$pdf->SetMargins(10, 20, 10);
+		$pdf->setHeaderMargin(10);
+		$pdf->setFooterMargin(0);
+
+		// headline for PDF
+		$pdf->setHeaderData('', '', $headline, '');
+
+		// set font
+		$pdf->SetFont('times', '', 10);
+
+		// add a page
+		$pdf->AddPage();
+
+		// Create table object for display
+		$table = new HtmlTable('history_table', null, $hoverRows, $datatable, $classTable);
+		$table->addAttribute('border', '1');
+		$table->addTableHeader();
+		$table->addRow();
+	}
+	elseif ($getMode === 'html')
+	{
+		$datatable = true;
+		$hoverRows = true;
+
+		// create html page object
+		$page = new HtmlPage();
+
+		if ($getFullScreen)
+		{
+			$page->hideThemeHtml();
+		}
+
+		$page->setTitle($title);
+		$page->setHeadline($headline);
+
+		// create filter menu with input elements for Startdate and Enddate
+		$FilterNavbar = new HtmlNavbar('menu_history_filter', null, null, 'filter');
+		$form = new HtmlForm('navbar_filter_form', ADMIDIO_URL . FOLDER_PLUGINS . $plugin_folder . '/history.php', $page, array('type' => 'navbar', 'setFocus' => false));
+		$form->addInput('filter_date_from', $gL10n->get('SYS_START'), $dateFromHtml, array('type' => 'date', 'maxLength' => 10));
+		$form->addInput('filter_date_to', $gL10n->get('SYS_END'), $dateToHtml, array('type' => 'date', 'maxLength' => 10));
+		$form->addInput('full_screen', '', $getFullScreen, array('property' => FIELD_HIDDEN));
+		$form->addSubmitButton('btn_send', $gL10n->get('SYS_OK'));
+		$FilterNavbar->addForm($form->show(false));
+		$page->addHtml($FilterNavbar->show());
+		
+		$page->addJavascript('
+            $("#export_list_to").change(function () {
+                if ($(this).val().length > 1) {
+                    var result = $(this).val();
+                    $(this).prop("selectedIndex",0);
+                    self.location.href = "'.ADMIDIO_URL.FOLDER_PLUGINS. $plugin_folder .'/history.php?" +
+                        "mode=" + result + "&filter_date_from='.$getDateFrom.'&filter_date_to='.$getDateTo.'";
+                }
+            });
+
+            $("#menu_item_print_view").click(function () {
+                window.open("'.ADMIDIO_URL.FOLDER_PLUGINS. $plugin_folder .'/history.php?mode=print&filter_date_from='.$getDateFrom.'&filter_date_to='.$getDateTo.'", "_blank");
+            });', true);
+
+		// get module menu
+		$listsMenu = $page->getMenu();
+		$listsMenu->addItem('menu_item_back', $gNavigation->getPreviousUrl(), $gL10n->get('SYS_BACK'), 'back.png');
+
+		if ($getFullScreen)
+		{
+			$listsMenu->addItem('menu_item_normal_picture', ADMIDIO_URL.FOLDER_PLUGINS. $plugin_folder .'/history.php?mode=html&amp;full_screen=false&amp;filter_date_from='.$getDateFrom.'&filter_date_to='.$getDateTo.'',
+					$gL10n->get('SYS_NORMAL_PICTURE'), 'arrow_in.png');
+		}
+		else
+		{
+			$listsMenu->addItem('menu_item_full_screen', ADMIDIO_URL.FOLDER_PLUGINS. $plugin_folder .'/history.php?mode=html&amp;full_screen=true&amp;filter_date_from='.$getDateFrom.'&filter_date_to='.$getDateTo.'',
+					$gL10n->get('SYS_FULL_SCREEN'), 'arrow_out.png');
+		}
+
+		// link to print overlay and exports
+		$listsMenu->addItem('menu_item_print_view', '#', $gL10n->get('LST_PRINT_PREVIEW'), 'print.png');
+
+		$form = new HtmlForm('navbar_export_to_form', '', $page, array('type' => 'navbar', 'setFocus' => false));
+		$selectBoxEntries = array(
+				''       => $gL10n->get('LST_EXPORT_TO').' ...',
+				'csv-ms' => $gL10n->get('LST_MICROSOFT_EXCEL').' ('.$gL10n->get('SYS_ISO_8859_1').')',
+				'pdf'    => $gL10n->get('SYS_PDF').' ('.$gL10n->get('SYS_PORTRAIT').')',
+				'pdfl'   => $gL10n->get('SYS_PDF').' ('.$gL10n->get('SYS_LANDSCAPE').')',
+				'csv-oo' => $gL10n->get('SYS_CSV').' ('.$gL10n->get('SYS_UTF8').')'
+		);
+		$form->addSelectBox('export_list_to', null, $selectBoxEntries, array('showContextDependentFirstEntry' => false));
+		$listsMenu->addForm($form->show(false));
+
+		$table = new HtmlTable('history_table', $page, $hoverRows, $datatable, $classTable);
+		$table->setDatatablesRowsPerPage($gPreferences['lists_members_per_page']);
+	}
+	else
+	{
+		$table = new HtmlTable('history_table', $page, $hoverRows, $datatable, $classTable);
+	}
+}
+
+//header definitions
 $columnHeading = array();
-
-$table->setDatatablesOrderColumns(array(array(5, 'desc')));
 $columnHeading[] = $gL10n->get('SYS_NAME');
 $columnHeading[] = $gL10n->get('PLG_MITGLIEDSBEITRAG_PAID_ON');
 $columnHeading[] = $gL10n->get('PLG_MITGLIEDSBEITRAG_DUEDATE');
 $columnHeading[] = $gL10n->get('PLG_MITGLIEDSBEITRAG_FEE');
 $columnHeading[] = $gL10n->get('PLG_MITGLIEDSBEITRAG_CONTRIBUTORY_TEXT');
 
-$table->addRowHeadingByArray($columnHeading);
+foreach($columnHeading as $headerData)
+{
+	if($getMode === 'csv')
+	{
+		$csvStr .= $separator. $valueQuotes. $headerData. $valueQuotes;
+	}
+	elseif($getMode === 'pdf')
+	{
+		$table->addColumn($headerData, array('style' => 'text-align: center;font-size:14;background-color:#C7C7C7;'), 'th');
+	}
+}
 
-$columnValues    = array();
+if($getMode === 'csv')
+{
+	$csvStr = substr($csvStr,strlen($separator))."\n";
+}
+elseif($getMode === 'html')
+{
+	$table->setColumnAlignByArray(array('left','left','left','left','left'));
+	$table->setDatatablesOrderColumns(array(array(5, 'desc')));
+	$table->addRowHeadingByArray($columnHeading);
+}
+elseif( $getMode === 'print')
+{
+	$table->setColumnAlignByArray(array('center','center','center','center','center'));
+	$table->setDatatablesOrderColumns(array(array(5, 'desc')));
+	$table->addRowHeadingByArray($columnHeading);
+}
+else
+{
+	$table->addTableBody();
+}
 
+$columnValues       = array();
 $presetColumnValues = array();
-$presetColumnValues[$gProfileFields->getProperty('PAID'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
-$presetColumnValues[$gProfileFields->getProperty('DUEDATE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
-$presetColumnValues[$gProfileFields->getProperty('FEE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
-$presetColumnValues[$gProfileFields->getProperty('CONTRIBUTORY_TEXT'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
+
+if ($getMode === 'csv')
+{
+	$presetColumnValues[$gProfileFields->getProperty('PAID'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '';
+	$presetColumnValues[$gProfileFields->getProperty('DUEDATE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '';
+	$presetColumnValues[$gProfileFields->getProperty('FEE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '';
+	$presetColumnValues[$gProfileFields->getProperty('CONTRIBUTORY_TEXT'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '';
+}
+else 
+{
+	$presetColumnValues[$gProfileFields->getProperty('PAID'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
+	$presetColumnValues[$gProfileFields->getProperty('DUEDATE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
+	$presetColumnValues[$gProfileFields->getProperty('FEE'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
+	$presetColumnValues[$gProfileFields->getProperty('CONTRIBUTORY_TEXT'.$gCurrentOrganization->getValue('org_id'), 'usf_id')] =  '&nbsp;';
+}
 
 while($row = $fieldHistoryStatement->fetch())
 {    
@@ -157,13 +360,88 @@ while($row = $fieldHistoryStatement->fetch())
    
    	$columnValues[$row['usl_usr_id']][$row['usl_usf_id']] = $gProfileFields->getHtmlValue($gProfileFields->getPropertyById((int) $row['usl_usf_id'], 'usf_name_intern'), $row['usl_value_new']);    
    	
-   	if( $row['usl_usf_id'] == $gProfileFields->getProperty('PAID'.$gCurrentOrganization->getValue('org_id'), 'usf_id') && $row['usl_value_new'] != '' && $row['usl_value_new'] >= $dateFromIntern && $row['usl_value_new'] <= $dateToIntern)
-   	{
-   		$columnValues[$row['usl_usr_id']] = array_merge(array('Name' => '<a href="'.ADMIDIO_URL.FOLDER_MODULES.'/profile/profile.php?user_id='.$row['usl_usr_id'].'">'.$row['last_name'].', '.$row['first_name'].'</a>'),$columnValues[$row['usl_usr_id']]);
-   		$table->addRowByArray($columnValues[$row['usl_usr_id']]);
+   	if(	$row['usl_usf_id'] == $gProfileFields->getProperty('PAID'.$gCurrentOrganization->getValue('org_id'), 'usf_id') 
+   		&& $row['usl_value_new'] != '' 
+   		&& $row['usl_value_new'] >= $dateFromIntern 
+   		&& $row['usl_value_new'] <= $dateToIntern )
+   	{  		
+   		//add last_name and first_name to the first column 
+   		array_unshift($columnValues[$row['usl_usr_id']], $row['last_name'].', '.$row['first_name']);
+   		
+   		if ($getMode === 'csv')
+   		{
+   			$csvTmp = '';
+   			foreach($columnValues[$row['usl_usr_id']] as $dummy => $data)
+   			{
+   				$csvTmp .= $separator.$valueQuotes.$data.$valueQuotes;
+   			}
+   			$csvStr .= substr($csvTmp,strlen($separator))."\n";
+   		}
+   		elseif($getMode === 'print' || $getMode === 'pdf')
+   		{
+   			$table->setColumnAlignByArray(array('center','center','center','center','center'));
+   			$table->addRowByArray($columnValues[$row['usl_usr_id']], null, array('nobr' => 'true'));
+   		}
+   		else
+   		{
+   			$table->addRowByArray($columnValues[$row['usl_usr_id']], null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. ADMIDIO_URL . FOLDER_MODULES . '/profile/profile.php?user_id='. $row['usl_usr_id']. '\''));
+   		}
    		unset($columnValues[$row['usl_usr_id']]);
    	}
 }
 
-$page->addHtml($table->show());
-$page->show();
+// Settings for export file
+if($getMode === 'csv' || $getMode === 'pdf')
+{
+	$filename .= '.'.$getMode;
+
+	// for IE the filename must have special chars in hexadecimal
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)
+	{
+		$filename = urlencode($filename);
+	}
+
+	header('Content-Disposition: attachment; filename="'.$filename.'"');
+
+	// neccessary for IE6 to 8, because without it the download with SSL has problems
+	header('Cache-Control: private');
+	header('Pragma: public');
+}
+
+if($getMode === 'csv')							// send the CSV-File to the user
+{
+	header('Content-Type: text/comma-separated-values; charset='.$charset);
+
+	if($charset == 'ISO-8859-1')
+	{
+		echo utf8_decode($csvStr);
+	}
+	else
+	{
+		echo $csvStr;
+	}
+}
+elseif($getMode === 'pdf')						// send the PDF-File to the User
+{
+	// output the HTML content
+	$pdf->writeHTML($table->getHtmlTable(), true, false, true, false, '');
+
+	//Save PDF to file
+	$pdf->Output(ADMIDIO_PATH . FOLDER_DATA . '/'.$filename, 'F');
+
+	//Redirect
+	header('Content-Type: application/pdf');
+
+	readfile(ADMIDIO_PATH . FOLDER_DATA . '/'.$filename);
+	ignore_user_abort(true);
+	unlink(ADMIDIO_PATH . FOLDER_DATA . '/'.$filename);
+}
+elseif($getMode === 'html' || $getMode === 'print')
+{
+	// add table list to the page
+	$page->addHtml($table->show(false));
+
+	// show complete html page
+	$page->show();
+}
+
