331e561ddf038827c48944bb23748fdcf9cbf18d
rmbinder <r-m-b@gmx.net>
2020-11-18 21:49:19 +0100

    fix 'a non-numeric value encountered'

--- history.php
+++ history.php
@@ -200,7 +200,7 @@ if ($getMode !== 'csv')
 		$pdf->setFooterMargin(0);
 
 		// headline for PDF
-		$pdf->setHeaderData('', '', $headline, '');
+		$pdf->setHeaderData('', 0, $headline, '');
 
 		// set font
 		$pdf->SetFont('times', '', 10);
