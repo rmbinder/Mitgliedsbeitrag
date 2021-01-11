c6d6c8d2a947db3b0144bfb9d8c31bcbb54bb005
rmbinder <r-m-b@gmx.net>
2021-01-11 19:58:26 +0100

    change 2020 to 2021

--- duedates.php
+++ duedates.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Setzen eines Faelligkeitsdatums fuer das Admidio-Plugin Mitgliedsbeitrag
  *
- * @copyright 2004-2020 The Admidio Team
+ * @copyright 2004-2021 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  *
