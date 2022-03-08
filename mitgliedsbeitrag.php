cc19f29a77977b6487f9da365150bae1472d24f8
rmbinder <r-m-b@gmx.net>
2022-03-08 17:28:08 +0100

    update version to 5.1.2

--- mitgliedsbeitrag.php
+++ mitgliedsbeitrag.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Mitgliedsbeitrag
  *
- * Version 5.1.1
+ * Version 5.1.2
  *
  * This plugin calculates membership fees based on role assignments.
  *
